513b172d4e88060d8b482ca6cf7bb882ca32edae
topDeveloper123 <jhahimanshu498@gmail.com>
2024-06-05 17:59:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/home/home';
+import About from './components/about/about';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/about/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './about.css'
+
+function about() {
+  return (
+    <div>
+      <h1 className='about'>About</h1>
+    </div>
+  )
+}
+
+export default about
