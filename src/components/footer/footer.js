372e288873951e82aa719c838b1dd6c922ddc045
jhahimanshu498@gmail.com <jhahimanshu498@gmail.com>
2024-06-06 18:50:40 +0530

    content and responsive header

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,85 @@
 import React from 'react'
+import './footer.css'
 
 function footer() {
   return (
     <div>
-      <h1>Footer</h1>
+      <div className='container-fluid main_box'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12 d-flex flex-wrap'>
+              <div className='col-lg-4 col-md-12 co-sm-12 border border-dark p-3'>
+                <p className='text-light mt-5'>Mayavi Fashion, the very name evokes images of ethereal beauty, avant-garde designs, and a seamless blend of tradition and modernity. As a leading force in the fashion industry, Mayavi transcends mere clothing to become a statement of individuality, elegance, and creativity. Origins and Inspiration: Mayavi Fashion draws its inspiration from diverse sources - from the rich cultural tapestry of India to the cutting-edge trends of international fashion capitals. Founded by visionary designers with a passion for innovation, Mayavi was born out of a desire to redefine conventional notions of style and luxur</p>
+                <p className='text-light'>Got Question? Call us 24/7</p>
+                <h5 className='text-light'>9262852851</h5>
+              </div>
+              <div className='col-lg-8 col-md-12 co-sm-12 border border-dark d-flex  p-4 flex-wrap px-5'>
+
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12'>
+                  <h6>Information</h6>
+                  <ul className="p-0">
+                    <li className='text-light'>About Us</li>
+                    <li className='text-light'>FAQ</li>
+                    <li className='text-light'>Terms & Conditions</li>
+                    <li className='text-light'>Contact Us</li>
+                    <li className='text-light'>Help</li>
+                    
+                  </ul>
+                </div>
+                
+
+
+
+
+
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12'>
+                <h6>Customer Service</h6>
+                  <ul className="p-0">
+                    <li className='text-light'>Payment Methods</li>
+                    <li className='text-light'>Money-back</li>
+                    <li className='text-light'>Returns</li>
+                    <li className='text-light'>Shipping</li>
+                    <li className='text-light'>Privacy Policy</li>
+                    
+                  </ul>
+                </div>
+                
+
+
+
+                <div className='col-lg-4 col-md-6 col-sm-6 col-12'>
+                <h6>Get In Touch</h6>
+                  <ul className="p-0">
+                    <p className='text-light'>355, 3rd Floor, Aggarwal Millennium Tower-1, Netaji Subhas Place, Pitam Pura, New Delhi - 110 034</p>
+                    <li className='text-light'>mayavifashion@gmail.com</li>
+                    <li className='text-light'>9262852851</li>
+                    
+                    
+                  </ul>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='container conatainer_Line mt-3'></div>
+
+      <div className='container mt-3'>
+        <div className='row'>
+          <div className='col-lg-8 col-md-12 col-12'>
+            <p class='text-light'>Copyright © 2024 | All Rights Reserved | Paras Parivaar Pvt Ltd. New Delhi</p>
+          </div>
+          <div className='col-lg-4 col-md-12 col-12 d-flex justify-content-end pe-5'>
+          <i class="bi bi-facebook mx-3 text-light"></i>
+          <i class="bi bi-instagram mx-3 text-light"></i>
+          <i class="bi bi-twitter-x mx-3 text-light"></i>
+          <i class="bi bi-youtube mx-3 text-light"></i>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      
     </div>
   )
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,69 +1,68 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import logo from './header_images/logo.png'
 import './header.css'
-function header() {
+
+function Header() {
+  const Li_show = useRef()
+
+  const Toggle_Menu =()=>{
+    Li_show.current.style.display = "flex"
+    Li_show.current.style.position = "absolute"
+    Li_show.current.style.top = "0"
+    Li_show.current.style.left = "0"
+    Li_show.current.style.width = "100%"
+    Li_show.current.style.height = "100%"
+    Li_show.current.style.background = "lightgreen"
+    Li_show.current.style.zIndex = "5"
+    
+  }
+
+  useEffect(()=>{
+    if (window.innerWidth < 992) {
+      
+      Li_show.current.style.display = "none"
+    }
+  }, [window.innerWidth])
+
+  const hide_Navbar = () =>{
+    
+      
+      Li_show.current.style.display = "none"
+    
+   
+    // Li_show.current.style.position = "inherit"
+    // Li_show.current.style.width = "revert-layer"
+    
+     
+  }
+
   return (
-    // <div className="container-fluid">
-    //   <div className='container'>
-    //     <div className='row w-100'>
+    <>
+  
 
-    //       <div className='col-12 border border-dark d-flex'>
-
-    //         {/* LOGO DIV */}
-    //         <div className='col-lg-2 col-md-2 col-sm-2 border border-dark'>
-    //           logo
-    //         </div>
-            
-    //         {/* LIST DIV */}
-    //         <div className='col-lg-5 col-md-5 col-sm-5 border border-dark'>
-    //           <ul className='border border-dark d-flex justify-content-around'>
-    //             <li><a href='void:javascript'>Home</a></li>
-    //             <li><a href='void:javascript'>Product</a></li>
-    //             <li><a href='void:javascript'>About</a></li>
-    //             <li><a href='void:javascript'>Contact</a></li>
-    //           </ul>
-    //         </div>
-
-    //         {/* SEARCH DIV */}
-    //         <div className='col-lg-4 col-md-4 col-sm-4 border border-dark h-100'>
-    //           <input type='text' className='w-75 h-100 bg-secondary '/>
-    //           <button className='btn btn-dark h-100 rounded-0'><i className="bi bi-search "></i></button>
-    //         </div>
-
-    //         {/* ICON DIV */}
-    //         <div className='col-lg-2 col-md-2 col-sm-2 border border-dark'>
-    //           icons
-    //         </div>
-
-    //       </div>
-
-    //     </div>
-    //   </div>
-    // </div>
-
-    <nav className="navbar navbar-expand-lg bg-body-secondary p-0 ">
-  <div className="container-fluid container-fluid-div p-0 h-100">
-    <div className='col-lg-2 col-md-2 col-5 border border-dark h-100 logo_div'>
+    <nav className="navbar navbar-expand-lg  p-0 position-static">
+  <div className="container-fluid container-fluid-div p-0 h-100 d-flex  align-items-center">
+    <div className='col-lg-2 col-md-2 col-3 border border-dark h-100 logo_div d-flex justify-content-around align-items-center'>
     <a className="navbar-brand" href="#"><img src={logo} /></a>
     </div>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
     
-    <div className='col-lg-4 col-md-4 col-4 border border-dark'>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+    
+    <div className='col-lg-4 col-md-2  col-2 border border-dark h-100  li_div' ref={Li_show}>
+    
+    <div className=" navbar-collapse" id="navbarSupportedContent">
+    <i class="bi bi-x fs-1  cross_icon" onClick={hide_Navbar}></i>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center w-100">
+        <li className="nav-item px-2">
+          <a className="nav-link  text-light" aria-current="page" href="#">HOME</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Products</a>
+        <li className="nav-item px-2">
+          <a className="nav-link text-light" href="#">PRODUCTS</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+        <li className="nav-item px-2">
+          <a className="nav-link text-light" href="#">ABOUT</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+        <li className="nav-item px-2">
+          <a className="nav-link text-light" href="#">CONTACT</a>
         </li>
       
         
@@ -71,21 +70,95 @@ function header() {
       </div>
       </div>
       
-      <div className='col-lg-4 col-md-4 col-4 border border-dark'>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+      <div className='col-lg-6  col-md-10 col-9 border border-dark h-100 d-flex align-items-center justify-content-center'>
+        <div className='col-lg-8  col-md-8 col-8 d-flex  justify-content-center '>
+      <form className="d-flex   col-lg-10 col-md-10 col-10" role="search">
+        <input className="form-control rounded-1 " type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success rounded-0 search_btn " type="submit"><i class="bi bi-search"></i></button>
       </form>
       </div>
+      <div className='col-lg-4  col-md-4 col-4 d-flex border border-dark justify-content-around icons_div '>
+      <div className=' d-flex position-relative w-100   justify-content-center align-items-center'>
+      <i class="bi bi-suit-heart" ></i>
+      <p className='text-light'>0</p>
+      </div>
 
-      <div className='col-lg-2 col-md-2 col-2 border border-dark h-100'>
-        icons
-        </div>
+      <div className='d-flex  position-relative  w-100 justify-content-center align-items-center'>
+      <i class="bi bi-cart3"></i>
+      <p className='text-light'>0</p>
+      </div>
+
+
+      <div className='d-flex  position-relative  w-100 justify-content-center align-items-center menu_div d-none'>
+      <i class="bi bi-list menu_icon" onClick={Toggle_Menu}></i>
+     
+      </div>
+      </div>
+      
+
+      </div>
+
+     
     </div>
     
   
 </nav>
+
+<div className='container-fluid'>
+  <div className='container'>
+    <div className='row category_main_div'>
+      <div className='col-12 d-flex justify-content-center'>
+      <div className='mt-3 mx-5 border border-dark text-center '>
+        <div className='mens_cate '>
+          <img src='https://t3.ftcdn.net/jpg/03/28/19/46/360_F_328194664_RKSHvMLgHphnD1nwQYb4QKcNeEApJmqa.jpg' alt=''/>
+          <h6 className='mt-2 m-0'>Men's</h6>
+          </div>
+          <div className='sub_categeries border border-dark w-75 '>
+
+            <div className='sub_mega col-4'>
+              <h4>Topwear</h4>
+              <ul className='p-0'>
+                <li>T-shirts</li>
+                <li>Shirts</li>
+              </ul>
+            </div>
+
+            <div className='sub_mega col-4'>
+              <h4>Bottomwear</h4>
+              <ul className='p-0'>
+                <li>Jeans</li>
+                <li>Shorts</li>
+              </ul>
+            </div>
+
+            <div className='sub_mega col-4'>
+              <h4>Footwear</h4>
+              <ul className='p-0'>
+                <li>Shoes</li>
+                <li>Sandel</li>
+              </ul>
+            </div>
+
+          </div>
+      </div>
+
+      <div className='mt-3 mx-5 text-center'>
+      <img src='https://st4.depositphotos.com/13193658/25036/i/450/depositphotos_250363326-stock-photo-smiling-attractive-woman-white-sweater.jpg' alt=''/>
+      <h6 className='mt-2'>Female's</h6>
+      </div>
+
+      <div className='mt-3 mx-5 text-center'>
+      <img src='https://img.freepik.com/premium-photo/small-4-year-old-boy-with-curly-hairstyle-stand-against-pastel-color-background_758367-99107.jpg' alt=''/>
+      <h6 className='mt-2'>Kid's</h6>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</>
+
   )
 }
 
-export default header
+export default Header
