c94b700798c93bf8b06c5caa03660d9e77d7571f
jhahimanshu498@gmail.com <jhahimanshu498@gmail.com>
2024-06-07 13:23:08 +0530

    header footer done

---
+++ src/components/Products/Products.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div>
+        <div class="container-fluid">
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+
+                        <div className='col-lg-3 col-md-3 col-sm-3 col-3  border border-dark'>
+                                ffdf
+                        </div>
+
+                        <div className='col-lg-9 col-md-9 col-sm-9 col-9 border border-dark '>
+dfd
+                        </div>
+
+                    </div>
+                </div>
+                </div>    
+        </div>
+      
+    </div>
+  )
+}
+
+export default Products
