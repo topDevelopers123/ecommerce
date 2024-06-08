642eb6ada850815e0c1337696b0a15a3b4022f81
topDevelopers123 <jhahimanshu498@gmail.com>
2024-06-08 17:55:07 +0530

    wishlist

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -8,35 +8,44 @@ function Wishlist() {
         <div>
 
             <div className='container'>
-                <div className='wish-content'>
 
-                    <div className='wish-list'>
+
+                <div className='wish-list p-0 w-100 '>
+                    <div className=' col-lg-2 text-center '>
                         <h6>PRODUCT</h6>
-                        <h6>NAME</h6>
-                        <h6>TOTAL</h6>
-                        <h6>ADD TO CART</h6>
-                        <h6><i class="bi bi-trash3"></i></h6>
                     </div>
+                    <div className='wish-text wish-text-1 text-center  col-lg-10 d-flex justify-content-between'>
+                        <h6 className='col-lg-3  col-12'>NAME</h6>
+                        <h6 className='col-lg-3  col-12'>TOTAL</h6>
+                        <h6 className='col-lg-3  col-12'>ADD TO CART</h6>
+                        <h6 className='col-lg-3  col-12'><i class="bi bi-trash3"></i></h6>
 
-                    <div className='wish-items'>
+                    </div>
+                </div>
+                <div className='wish-items text-center w-100 '>
+                    <div className=' col-lg-2 col-md-5 col-sm-5 col-5'>
                         <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ815kJ-v7Uks_o0V9j0SHxU-2m79ugH17qIC0az_TCbowEaenkpmcqcMS9jMwI4QnldKcEw5rp7kp9NnTa1AUPa9w8aWT4xj2MrL8tmLI' />
-                        <Link >EYEBOGLER Polo T-shirt For Men</Link>
-                        <h6>$ 199.00</h6>
-                        <button>ADD TO CART</button>
-                        <h6><i class="bi bi-trash3"></i></h6>
                     </div>
+                    <div className='wish-text wish-text-2   col-lg-10 col-md-7 col-sm-7 col-7 d-flex justify-content-between '>
+                        <Link className='col-lg-3 col-md-12 col-sm-12 '>EYEBOGLER Polo T-shirt For Men</Link>
+                        <h6 className='col-lg-3 col-md-12 col-sm-12 '>$ 199.00</h6>
+                        <div className='col-lg-3 col-md-12 col-sm-12 '>
+                            <button >ADD TO CART</button></div>
+                        <h6 className='col-lg-3 col-md-12 col-sm-12 '><i class="bi bi-trash3"></i></h6>
+                    </div>
+                </div>
 
-                    <div className='wish-items'>
+                {/* <div className='wish-items'>
                         <img src='https://shrus.com/cdn/shop/files/cbs02150.jpg?v=1712667433&width=3024' />
                         <Link >EYEBOGLER Polo T-shirt For Men</Link>
                         <h6>$ 199.00</h6>
                         <button>ADD TO CART</button>
                         <h6><i class="bi bi-trash3"></i></h6>
-                    </div>
+                    </div> */}
 
 
-                </div>
             </div>
+
         </div>
     )
 }
