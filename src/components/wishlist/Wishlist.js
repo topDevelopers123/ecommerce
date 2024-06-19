7701f44ab6cb7cd672ecc312703a9ff3961dd4ef
topDevelopers123 <jhahimanshu498@gmail.com>
2024-06-19 18:50:46 +0530

    checkout

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -25,7 +25,7 @@ function Wishlist() {
                     </div>
                     <div className='wish-items-shadow'>
                         <div className='wish-items text-center w-100 '>
-                            <div className=' col-lg-2 col-md-5 col-sm-5 col-6'>
+                            <div className=' col-lg-2 col-md-5 col-sm-5 col-6 img-fluid'>
                                 <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ815kJ-v7Uks_o0V9j0SHxU-2m79ugH17qIC0az_TCbowEaenkpmcqcMS9jMwI4QnldKcEw5rp7kp9NnTa1AUPa9w8aWT4xj2MrL8tmLI' />
                             </div>
                             <div className='wish-text wish-text-3 col-lg-10 col-md-7 col-sm-7 col-6 d-flex justify-content-between align-items-center'>
