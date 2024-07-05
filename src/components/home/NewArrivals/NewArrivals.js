348a14248236e1b6243cd55a8485f4a70b5055e8
topDevelopers123 <jhahimanshu498@gmail.com>
2024-07-05 13:53:31 +0530

    Arrival data

--- src/components/home/NewArrivals/NewArrivals.js
+++ src/components/home/NewArrivals/NewArrivals.js
@@ -1,27 +1,145 @@
-import React, { useState } from 'react'
+// import React from 'react'
+// import OwlCarousel from "react-owl-carousel";
+// import "owl.carousel/dist/assets/owl.carousel.css";
+// import "owl.carousel/dist/assets/owl.theme.default.css";
+// import { useProductContext } from '../../../Context/index.context';
+// import { useNavigate } from 'react-router-dom';
+
+
+
+// function ShopByCategory() {
+//     const { productData } = useProductContext();
+//     console.log(productData)
+
+//     const navigate = useNavigate()
+
+//     const ArrivalProductNavigate = (id) => {
+//         navigate(`/ArrivalProductNavigate/${id}`)
+//         window.scrollTo(0, 0);
+//     }
+
+//     return (
+//         <div>
+//             {/* New Arrivals start */}
+//             <section className="new_arrivals">
+//                 <div className="container">
+//                     <div className="head_title">
+//                         <h2>New Arrivals</h2>
+//                         <div className="bdr"></div>
+//                     </div>
+//                     <div className="categories" >
+
+//                         <OwlCarousel
+//                             className="owl-theme"
+//                             loop
+//                             margin={10}
+//                             nav={false} // Hide navigation arrows
+//                             dots={false} // Hide dots
+//                             autoplay
+//                             autoplayTimeout={5000}
+//                             items={4} // Number of items to display
+//                             responsive={{
+//                                 0: {
+//                                     items: 3, // 1 item in mobile view
+//                                 },
+//                                 768: {
+//                                     items: 4, // 3 items in tablet view
+//                                 },
+//                                 1200: {
+//                                     items: 4, // 4 items in desktop view
+//                                 },
+//                             }}
+//                         >
+
+//                             {productData?.map((item, i) =>
+//                                 <div key={i} className="item" >
+
+//                                     <img src={item?.ProductDetails[0]?.image[0]?.image_url} alt={item?.product_title} onClick={() => ArrivalProductNavigate(item?._id)} />
+//                                     <h4>{productData[i]?.title}</h4>
+//                                 </div>
+//                             )}
+
+//                         </OwlCarousel>
+//                         <OwlCarousel
+//                             className="owl-theme"
+//                             loop
+//                             margin={10}
+//                             nav={false} // Hide navigation arrows
+//                             dots={false} // Hide dots
+//                             autoplay
+//                             autoplayTimeout={5000}
+//                             items={4} // Number of items to display
+//                             responsive={{
+//                                 0: {
+//                                     items: 3, // 1 item in mobile view
+//                                 },
+//                                 768: {
+//                                     items: 4, // 3 items in tablet view
+//                                 },
+//                                 1200: {
+//                                     items: 4, // 4 items in desktop view
+//                                 },
+//                             }}
+//                         >
+//                             {productData?.map((item, i) =>
+//                                 <div key={i} className="item" >
+
+//                                     <img src={item?.ProductDetails[0]?.image[0]?.image_url} alt={item?.product_title} onClick={() => ArrivalProductNavigate(item?._id)} />
+//                                     <h4>{productData[i]?.title}</h4>
+//                                 </div>
+//                             )}
+
+//                         </OwlCarousel>
+//                     </div>
+
+//                 </div>
+//             </section>
+//             {/* New Arrivals end  */}
+//         </div>
+//     )
+// }
+
+// export default ShopByCategory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import sbc01 from "../img/category/men_casual.jpg";
-import sbc02 from "../img/category/men_formal.webp";
-import sbc03 from "../img/category/women_casual.jpg";
-import sbc04 from "../img/category/women_ethnic.webp";
 import { useProductContext } from '../../../Context/index.context';
 import { useNavigate } from 'react-router-dom';
 
-
-
 function ShopByCategory() {
     const { productData } = useProductContext();
-    console.log(productData)
+    console.log(productData);
 
-    const [allProductData, setAllProductData] = useState(productData)
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const ArrivalProductNavigate = (id) => {
-        navigate(`/ArrivalProductNavigate/${id}`)
+        navigate(`/ArrivalProductNavigate/${id}`);
         window.scrollTo(0, 0);
-    }
+    };
+
+    // Get the first 4 items
+    const firstFourProducts = productData?.slice(0, 4);
+    // Get the last 4 items
+    const lastFourProducts = productData?.slice(4,8);
 
     return (
         <div>
@@ -32,8 +150,7 @@ function ShopByCategory() {
                         <h2>New Arrivals</h2>
                         <div className="bdr"></div>
                     </div>
-                    {/* {allProductData && allProductData?.map((item) => */}
-                    <div className="categories" >
+                    <div className="categories">
 
                         <OwlCarousel
                             className="owl-theme"
@@ -46,40 +163,24 @@ function ShopByCategory() {
                             items={4} // Number of items to display
                             responsive={{
                                 0: {
-                                    items: 3, // 1 item in mobile view
+                                    items: 3, // 3 items in mobile view
                                 },
                                 768: {
-                                    items: 4, // 3 items in tablet view
+                                    items: 4, // 4 items in tablet view
                                 },
                                 1200: {
                                     items: 4, // 4 items in desktop view
                                 },
                             }}
                         >
-
-                            {productData?.map((item, i) =>
-                                <div key={i} className="item" >
-
+                            {firstFourProducts?.map((item, i) => (
+                                <div key={i} className="item">
                                     <img src={item?.ProductDetails[0]?.image[0]?.image_url} alt={item?.product_title} onClick={() => ArrivalProductNavigate(item?._id)} />
-                                    {console.log(item)}
-                                    <h4>Men's Causal Wear</h4>
+                                    <h4>{firstFourProducts[i]?.title}</h4>
                                 </div>
-                            )}
-
-                            {/* <div className="item">
-                                <img src={sbc02} alt='' />
-                                <h4>Men's Formal Wear</h4>
-                            </div>
-                            <div className="item">
-                                <img src={sbc03} alt='' />
-                                <h4>Category 1</h4>
-                            </div>
-                            <div className="item">
-                                <img src={sbc04} alt='' />
-                                <h4>Category 1</h4>
-                            </div> */}
-
+                            ))}
                         </OwlCarousel>
+
                         <OwlCarousel
                             className="owl-theme"
                             loop
@@ -91,40 +192,30 @@ function ShopByCategory() {
                             items={4} // Number of items to display
                             responsive={{
                                 0: {
-                                    items: 3, // 1 item in mobile view
+                                    items: 3, // 3 items in mobile view
                                 },
                                 768: {
-                                    items: 4, // 3 items in tablet view
+                                    items: 4, // 4 items in tablet view
                                 },
                                 1200: {
                                     items: 4, // 4 items in desktop view
                                 },
                             }}
                         >
-                            <div className="item">
-                                <img src={sbc01} alt='' />
-                                <h4>Men's Causal Wear</h4>
-                            </div>
-                            <div className="item">
-                                <img src={sbc02} alt='' />
-                                <h4>Men's Formal Wear</h4>
-                            </div>
-                            <div className="item">
-                                <img src={sbc03} alt='' />
-                                <h4>Category 1</h4>
-                            </div>
-                            <div className="item">
-                                <img src={sbc04} alt='' />
-                                <h4>Category 1</h4>
-                            </div>
+                            {lastFourProducts?.map((item, i) => (
+                                <div key={i} className="item">
+                                    <img src={item?.ProductDetails[0]?.image[0]?.image_url} alt={item?.product_title} onClick={() => ArrivalProductNavigate(item?._id)} />
+                                    <h4>{item?.product_title}</h4>
+                                </div>
+                            ))}
                         </OwlCarousel>
-                    </div>
 
+                    </div>
                 </div>
             </section>
-            {/* New Arrivals end  */}
+            {/* New Arrivals end */}
         </div>
-    )
+    );
 }
 
-export default ShopByCategory
+export default ShopByCategory;
